bb3020adc2977e45615aecd33e638248422b3572
Antônio Paes <paesdeandradeantonio@gmail.com>
2024-11-26 23:27:17 -0300

    Resolvendo testes

--- Plantage/cypress/e2e/4adicionarplantaaocanteiro.cy.js
+++ Plantage/cypress/e2e/4adicionarplantaaocanteiro.cy.js
@@ -10,10 +10,10 @@ describe('Adicionar Hortaliça ao canteiro', () => {
         
         //Adicionar Hortaliça ao canteiro
         cy.get('[id^="icon-"]').first().click() // Pega o primeiro ícone que começa com "icon-"
-        cy.contains('Adicionar Planta').click() // Encontra pelo texto
+        cy.contains('Minhas Plantas').click() // Encontra pelo texto
         cy.get('[id^="planta-"]').first().click() // Pega a primeira planta
         cy.get('input[type="number"]').first().type('2') // Pega o primeiro input numérico
-        cy.get('button').contains('Adicionar').click() // Encontra botão pelo texto
+        cy.contains('Adicionar Hortaliça ao canteiro').click() // Encontra botão pelo texto
         cy.get('[id^="icon-"]').first().click() // Fecha o menu
     })
 })
